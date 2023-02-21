e1b9c07b5005e73c641068d91055d55ba05add6d
DorianDesings <35946850+DorianDesings@users.noreply.github.com>
2023-02-21 18:21:07 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+export default defineConfig({
+  plugins: [ViteMinifyPlugin({}), ViteImageOptimizer({})],
+  build: {
+    outDir: 'docs'
+  }
+});
